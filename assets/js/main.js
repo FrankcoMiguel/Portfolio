6e2a30c3a8160ef0c9c8e4dd4832db4f8a9e50e6
Frank Orozco <frank_miguel29@outlook.com>
2020-01-13 11:10:35 -0900

    vue component created -> project card component

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,39 @@
-Vue.component('project-name', {
+Vue.component('project-card', {
 
-    props: [img, project-name, description, link],
+    props: ['img','width', 'title','description','version', 'link'],
+    template: `
+    
 
+        <div class="div-project">
+
+          <div class="wrapper-project">
+
+            <img v-bind:src="img" v-bind:width="width" height="auto">
+
+            <h3>{{ title }}</h3>
+
+            <div class="project-paragraph text-left">
+
+              <p class="h5 justify-content-left">{{ description }}
+              </p>
+
+              <h5 class="mt-4">Actual Version: {{ version }}</h5>
+
+              <div class="row justify-content-end">
+                <a v-bind:href="link"><img src="assets/images/contact/github.svg"></a>
+              </div>
+
+            </div>
+
+          </div>
+
+        </div>
+    
+    `
 })
+
+new Vue({
+
+    el: '#app'
+
+});
