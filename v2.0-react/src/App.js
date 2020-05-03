8568784c18528647df94dffcb8b559a588b51cb5
Frank Orozco <frank_miguel29@outlook.com>
2020-05-03 07:32:39 -0800

    eslint good practices applied and app architecture changed

---
+++ v2.0-react/src/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <h1> Routing, Components and Containers </h1>
+    );
+  }
+}
+
+export default App;
--- v2.0-react/src/index.js
+++ v2.0-react/src/index.js
@@ -1,10 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Header from './components/Header'
+import App from './App';
 
-ReactDOM.render(
-
-    <Header /> 
-    
-    
-    ,document.getElementById("react-app"))
+ReactDOM.render(<App />, document.getElementById('portfolio'));
