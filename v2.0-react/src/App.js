945df64da687ec80418c094200b4e7a4ca0ba047
Frank Orozco <frank_miguel29@outlook.com>
2020-05-19 04:39:00 -0800

    Header component added to App.js

--- v2.0-react/src/App.js
+++ v2.0-react/src/App.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
+import Header from './containers/Header';
 
 class App extends Component {
   render() {
     return (
-      <h1> Routing, Components and Containers </h1>
+      <Header />
     );
   }
 }
