ff40b8b54db34fc510f5d1157e5ed9352f5decb8
Frank Orozco <frank_miguel29@outlook.com>
2020-06-11 00:14:03 -0800

    Skills data set

---
+++ src/data/SkillsData.js
@@ -0,0 +1,55 @@
+
+const SkillsData = [
+    {
+        id: 1,
+        title: "Web Design",
+        description: "Great knowledge constructing and designing beautiful but usable user interfaces using CSS Frameworks and Preprocessors.",
+        image: '../../src/assets/static/sass-icon.svg',
+        imageAlt: "Sass Icon"
+    },
+
+    {
+        id: 2,
+        title: "Frontend Development",
+        description: "Excellent understanding of JavaScript ES9, experienced creating Reactive Components with React.js Library.",
+        image: '../../src/assets/static/react-icon.svg',
+        imageAlt: "React Js Icon"
+    },
+
+    {
+        id: 3,
+        title: "Backend Development",
+        description: "Experienced in Microsoft Technologies and Java Frameworks, great knowledge in REST API and Web Services projects.",
+        image: '../../src/assets/static/dotnet-icon.svg',
+        imageAlt: "Dot Net Icon"
+    },
+
+    {
+        id: 4,
+        title: "Database Administration",
+        description: "Good knowledge constructing and maintaining Relational and NoSQL Databases, such as SQL Server, MySQL, Oracle and MongoDB.",
+        image: '../../src/assets/static/db-icon.svg',
+        imageAlt: "Database Icon"
+         
+    },
+
+    {
+        id: 5,
+        title: "Teamwork Ability",
+        description: "Nice skills and qualities to work with others developers, well communication and responsability.",
+        image: '../../src/assets/static/teamwork-icon.svg',
+        imageAlt: "People Icon"
+    },
+
+    {
+        id: 6,
+        title: "Software Design Principles",
+        description: "Experience implementing the best software practices, such as SOLID Principles and Design Patterns.",
+        image: '../../src/assets/static/sbp-icon.svg',
+        imageAlt: "Software Design Principles"
+    }
+
+
+]
+
+export default SkillsData;
