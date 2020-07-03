1d0304cb21e749674a95926ba514edf148c3fcda
Frank Orozco <frank_miguel29@outlook.com>
2020-07-03 06:43:21 -0800

    Project section created and Boxes View components integrated

--- src/containers/Projects.jsx
+++ src/containers/Projects.jsx
@@ -5,11 +5,15 @@ import '../assets/styles/Projects.scss';
 //Icons
 import Logotype from '../assets/static/logotype.svg';
 //Components
+import BoxView from '../components/BoxView';
 
 //Data
 import ProjectsData from '../data/ProjectsData';
 
 const Projects = () => {
+
+    const BoxesView = ProjectsData.map(project => <BoxView key={project.id} image={project.logotype} imageAlt={project.logotypeAlt} width='238px;' />) //This an Array of BoxView Components using the project data.
+
     return (
         <section>
             <Container>
@@ -17,6 +21,10 @@ const Projects = () => {
                     <Col xs={12}><Image src={Logotype} alt="Logotype" /></Col>
                     <Col xs={12}><h1>Personal Projects</h1></Col>
                     <Col xs={12}><p>Here are few personal projects I've worked recently. Questions? <a href='./'>Contact me</a></p></Col>
+                </Row>
+
+                <Row>
+                    {BoxesView}
                 </Row>
 
 
