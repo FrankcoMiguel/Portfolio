988e97d3820d4f13404443a566164e5986de222b
Frank Orozco <frank_miguel29@outlook.com>
2020-08-19 22:00:46 -0800

    Navigation Bar component updated

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -15,14 +15,16 @@ const NavigationBar = () => {
             <Row className="NavContainer">
                 <Link to="/"><Image className="NavLogo" src={Logotype} /></Link>
                 <ul className="NavContent">
-                    <li className="NavItem"><Link to='/'>About me</Link></li>
+                    <li className="NavItem"><Link to='/404'>About me</Link></li>
                     <li className="NavItem"><Link to='/'>Portfolio</Link></li>
                     <li className="NavItem"><Link to='/'>Blog</Link></li>
                     <li className="NavItem"><Link to='/'>Contacts</Link></li>
                     <li className="NavDivider"></li>
-                    <li className="NavLink"><a href="https://github.com/FrankcoMiguel" target="_blank"><Image src={GithubIcon} /></a></li>
-                    <li className="NavLink"><a href="https://www.instagram.com/franco.js/" target="_blank"><Image src={InstagramIcon} /></a></li>
-                    <li className="NavLink"><a className="https://www.linkedin.com/in/frank-orozco-2a41b2185/" target="_blank"><Image src={LinkedinIcon} /></a></li>
+                    <div className="NavIcons">
+                        <li className="NavLink"><a href="https://github.com/FrankcoMiguel" target="_blank"><Image src={GithubIcon} /></a></li>
+                        <li className="NavLink"><a href="https://www.instagram.com/franco.js/" target="_blank"><Image src={InstagramIcon} /></a></li>
+                        <li className="NavLink"><a href="https://www.linkedin.com/in/frank-orozco-2a41b2185/" target="_blank"><Image src={LinkedinIcon} /></a></li>
+                    </div>
                 </ul>
             </Row>
         </nav>
