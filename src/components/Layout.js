a7a23b09de0b0550326bc114fb55364246136993
Frank Orozco <frank_miguel29@outlook.com>
2020-08-19 21:59:24 -0800

    Layout styles created

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import NavigationBar from './Navbar'
+import '../styles/layout.scss'
 
 
 const Layout = (props) => {
     return (
         <div className="Body">
             <NavigationBar />
-            {props.children}
+                {props.children}  
         </div>
     )
     
