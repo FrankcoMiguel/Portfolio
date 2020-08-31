ad9f3d3d75990b7ea38bbcd670be83b7cd62cb99
Frank Orozco <frank_miguel29@outlook.com>
2020-08-31 00:26:53 -0800

    mobile styles removed

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import NavigationBar from './Navbar'
-import '../styles/layout.scss'
+import '../styles/Layout.scss'
 
 
 const Layout = (props) => {
