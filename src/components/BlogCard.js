6b5d3700d0bf5491bb9d8707140672804dba4490
Frank Orozco <frank_miguel29@outlook.com>
2020-09-02 02:00:00 -0800

    BlogCard Component created

---
+++ src/components/BlogCard.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Container, Image } from 'react-bootstrap'
+import '../styles/BlogCard.scss'
+
+
+const BlogCard = (props) => {
+    return (
+        <Container style={{backgroundImage: `url(${props.background})` }} className="BlogCardContainer">
+            <div className="BlogCardContent">
+                <h2 className="BlogCardTitle">{props.title}</h2>
+                <p className="BlogCardDetail">{props.paragraph}</p>
+            </div>
+        </Container>
+    )
+}
+
+export default BlogCard
