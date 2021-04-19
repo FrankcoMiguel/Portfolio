ee345ec2e6762ea62ed453698d1cb20310c93996
FrankcoMiguel <Frank_Miguel29@outlook.com>
2021-04-18 20:38:30 -0800

    Footer tag added in Footer component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,7 +7,7 @@ import Tippy from '@tippyjs/react'
 
 const Footer = () => {
     return(
-        <div className="Footer">
+        <footer>
             <p>®Frank Orozco 2019 - 2021</p>
             <div className="Row">
                 <p>All rights reserved. Built with</p>
@@ -15,7 +15,7 @@ const Footer = () => {
                 <p>using</p>
                 <Tippy content="GatsbyJS" placement="right"><a href='https://www.gatsbyjs.com/'><i className="devicon-gatsby-plain colored"></i></a></Tippy>
             </div>
-        </div>
+        </footer>
     )
 }
 
