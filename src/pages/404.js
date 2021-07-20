c75d414f5a876214903608286375fa3e9bd81f7f
FrankcoMiguel <Frank_Miguel29@outlook.com>
2021-07-19 23:46:58 -0800

    NotFound page done

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,12 +1,26 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Layout from '../components/Layout'
+import Image from '../images/404.png'
+import Aos from 'aos'
+import 'aos/dist/aos.css'
+import { Link } from 'gatsby'
+import './404.scss'
 
 export default function NotFound() {
-    return (
-      <Layout>
+
+  useEffect(() => {
+    Aos.init({ duration: 900 })
+  },[])
+
+  return (
+    <Layout>
+      <div className="NotFound">
+        <img data-aos="zoom-in" src={Image}></img>
         <h1>Page Not Found</h1>
         <p>Oops, we couldn't find this page!</p>
-      </Layout>
-    )
-  }
+        <Link to="/">Return to home</Link>
+      </div>
+    </Layout>
+  )
+}
   
