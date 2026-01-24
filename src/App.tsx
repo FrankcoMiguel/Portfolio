15184b77523a4b5cfe6c5dd62968d81b9ff1e941
Frank Orozco <Frank_Miguel29@outlook.com>
2026-01-24 02:39:28 -0500

    when page open, auto scrolls to the top

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
-import { BrowserRouter as Router, Routes, Route, Navigate, useParams } from 'react-router-dom';
+import { useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate, useParams, useLocation } from 'react-router-dom';
 import Home from './pages/Home';
 import Experiments from './pages/Experiments';
 import ExperimentDetail from './pages/ExperimentDetail';
 import Publications from './pages/Publications';
 import PublicationDetail from './pages/PublicationDetail';
 import NotFound from './pages/NotFound';
+
+// Scroll to top on route change
+const ScrollToTop = () => {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+};
 
 // Redirect component for dynamic routes
 const ProjectRedirect = () => {
@@ -15,6 +27,7 @@ const ProjectRedirect = () => {
 function App() {
   return (
     <Router>
+      <ScrollToTop />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/experiments" element={<Experiments />} />
